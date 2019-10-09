83ba7af508dd4fdb45d592ef3b4195678ff6bc87
Osan Gordon <oagordon@asu.edu>
2019-10-08 18:43:47 -0700

    Final commit.

--- CSE360/src/cse360assign2/AddingMachine.java
+++ CSE360/src/cse360assign2/AddingMachine.java
@@ -4,14 +4,15 @@
  * Assignment 2
  * 
  * This function can add or subtract amounts from the 'total'
- * variable. It can also display the current total and keep a
- * history of all the transactions.
+ * variable. It can also display the current total, as well
+ * as keep and display the history of all transactions.
  */
 package cse360assign2;
 
 public class AddingMachine {
 
 	private int total;
+	private String history = "0";
 	
 	/**
 	 * Constructor for the 'total' variable
@@ -25,30 +26,34 @@ public class AddingMachine {
 	 * @return the current value of 'total'
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	/**
 	 * Adds an integer 'value' to 'total'
+	 * Updates 'history' to keep track of transactions
 	 * @param value the amount to be added to 'total'
 	 */
 	public void add (int value) {
-		
+		total += value;
+		history += " + " + value;
 	}
 	
 	/**
 	 * Subtracts an integer 'value' from 'total'
+	 * Updates 'history' to keep track of transactions
 	 * @param value the amount to be subtracted from 'total'
 	 */
 	public void subtract (int value) {
-		
+		total -= value;
+		history += " - " + value;
 	}
 	
 	/**
 	 * Returns a history of the transactions	
 	 */
 	public String toString () {
-		return "";
+		return history;
 	}
 
 	public void clear() {
